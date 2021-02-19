bef645f86a29cea7b8496f7067ec82ed0dee0672
chipfody <60615532+chipfody@users.noreply.github.com>
2021-02-19 14:22:26 -0500

    cert - more loops

--- src/dataTypes/WorkingWithMethods.java
+++ src/dataTypes/WorkingWithMethods.java
@@ -149,3 +149,16 @@ class Loops {
         System.out.print(x + " ");
     }
 }
+
+class SwitchSample {
+    public static void main(String[] args) {
+        FIRST_CHAR_LOOP: for (int a = 1; a <= 4; a++) {
+            for (char x = 'a'; x <= 'c'; x++) {
+                if (a == 2 || x == 'b')
+                    continue FIRST_CHAR_LOOP;
+                System.out.print(" " + a + x);
+            }
+        }
+    }
+}
+
